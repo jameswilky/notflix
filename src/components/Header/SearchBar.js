bfed385d65e902cbd70ade090c072dd17ebd65fc
James Wilkinson <jdwilkinson1994@gmail.com>
2019-05-15 13:25:59 +0800

    added animations to searchbar

--- src/components/Header/SearchBar.js
+++ src/components/Header/SearchBar.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./SearchBar.module.css";
 
 export default function SearchBar() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const clickedAway = e => {
+      if (show && e.target.id != "search") {
+        setShow(!show);
+      }
+    };
+    document.addEventListener("click", clickedAway);
+
+    return () => document.removeEventListener("click", clickedAway);
+  });
   return (
-    <div className={`${styles.container} ${styles.hideSearch}`}>
+    <div
+      className={`${styles.container} ${show ? styles.show : ""}`}
+      onClick={() => setShow(!show)}
+    >
       <i className={`fas fa-search ${styles.searchBtn}`} onClick={() => {}} />
-      <input
-        className={styles.input}
-        type="text"
-        placeholder="Titles, people, genres"
-      />
+      {show ? (
+        <input
+          id={"search"}
+          onClick={e => e.stopPropagation()}
+          className={styles.input}
+          type="text"
+          placeholder="Titles, people, genres"
+          autoFocus
+        />
+      ) : null}
     </div>
   );
 }
