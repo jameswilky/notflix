8edd5bc4d50cda6c359c4f957cced2f16ec49556
James Wilkinson <jdwilkinson1994@gmail.com>
2019-05-28 12:21:31 +0800

    completed loading animation

--- src/components/Browser/Browser.js
+++ src/components/Browser/Browser.js
@@ -9,15 +9,16 @@ import pageNames from "../../pageNames";
 import { AuthContext } from "../../contexts/AuthContext";
 
 export default function Browser(props) {
+  const { FAVOURITES, SEARCH } = pageNames;
+
   const { includeBanner, videoType = false, content } = props;
   const { videosLoaded, videosByGenre } = useVideos();
-  const { FAVOURITES } = pageNames;
-
   const [likedVideos, setLikedVideos] = useState([]);
-
   const { auth } = useContext(AuthContext);
+
   const query = props.history.location.search;
   const userId = "google-oauth2|103091392578361804114";
+
   useEffect(() => {
     fetch(`/list/${userId}`)
       .then(response => {
@@ -26,53 +27,68 @@ export default function Browser(props) {
       })
       .then(response => {
         setLikedVideos(response);
-        console.log(videosByGenre);
       });
   }, []);
 
   const BrowserBody = () => {
-    return videosLoaded ? (
-      videosByGenre.map(items => {
-        const genre = Object.keys(items)[0];
-        let videos = Object.values(items)[0];
-        //if type specified
-        if (videoType) {
-          // Only include videos of specified type
-          videos = items[genre].filter(video => {
-            return video.type === videoType;
-          });
-        }
-        if (videos.length > 5) {
-          return (
-            <Carousel genre={genre} videos={videos} key={uuid()} {...props} />
-          );
-        } else return null;
-      })
-    ) : (
-      <Loading />
+    return videosByGenre.map(items => {
+      const genre = Object.keys(items)[0];
+      let videos = Object.values(items)[0];
+      //if type specified
+      if (videoType) {
+        // Only include videos of specified type
+        videos = items[genre].filter(video => {
+          return video.type === videoType;
+        });
+      }
+      if (videos.length > 5) {
+        return (
+          <Carousel genre={genre} videos={videos} key={uuid()} {...props} />
+        );
+      } else return null;
+    });
+  };
+  const Favourites = () => {
+    return (
+      <>
+        {" "}
+        <div className={styles.container} />
+        <div>
+          <Carousel
+            genre={"MyList"}
+            videos={likedVideos}
+            key={uuid()}
+            {...props}
+          />
+        </div>
+      </>
     );
+  };
+  const SearchResults = () => {
+    return BrowserBody();
+  };
+  const Body = () => {
+    switch (content) {
+      case FAVOURITES:
+        return Favourites();
+      case SEARCH:
+        return SearchResults(); // Temp
+      default:
+        return BrowserBody();
+    }
   };
 
   return (
     <div className={styles.main}>
-      {content === FAVOURITES ? (
-        <>
-          <div className={styles.container} />
-          <div>
-            <Carousel
-              genre={"MyList"}
-              videos={likedVideos}
-              key={uuid()}
-              {...props}
-            />
-          </div>
-        </>
-      ) : (
+      <Loading />
+      {/* {videosLoaded ? (
         <>
           {includeBanner ? <BrowserHeader /> : null}
-          <div className={styles.carouselContainer}>{<BrowserBody />}</div>{" "}
+          <div className={styles.carouselContainer}>{<Body />}</div>{" "}
         </>
-      )}
+      ) : (
+        <Loading />
+      )} */}
     </div>
   );
 }
--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,16 +1,14 @@
 import React from "react";
+import userIcon from "../../images/userIcon.jpg";
+import styles from "./Loading.module.css";
 
 export default function Loading() {
   return (
-    <div
-      style={{
-        height: "100vh",
-        backgroundColor: "var(--black)",
-        padding: "25% 50% 25% 50%"
-      }}
-    >
-      {" "}
-      Loading Content{" "}
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.ring} />
+        <img className={styles.logo} src={userIcon} alt="" />
+      </div>
     </div>
   );
 }
