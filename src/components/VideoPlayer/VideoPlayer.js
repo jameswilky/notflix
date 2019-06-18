e44f12c9dea688a131a9b069a327ee946dd0b797
James Wilkinson <jdwilkinson1994@gmail.com>
2019-06-18 19:29:59 +0800

    reverted thumbnail changes

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -7,9 +7,11 @@ export default function VideoPlayer(props) {
   const { video, position, width, style = {}, type, autoplay = false } = props;
   const { videoId } = video;
   const [player, setPlayer] = useState();
-  const [showThumbnail, setShowThumbnail] = useState(true);
+  const [showThumbnail, setShowThumbnail] = useState(!autoplay);
   const [loadPlayer, setLoadPlayer] = useState(false);
-
+  const playerLoading = new Promise(resolve => {
+    if (player !== undefined) resolve(player);
+  });
   const [metaData] = useState({
     title: video.title,
     match: `${video.vote_average * 10}% Match`, // use user score
@@ -19,7 +21,13 @@ export default function VideoPlayer(props) {
     overview: video.overview
   });
   const thumbnailRef = React.useRef();
-
+  useEffect(() => {
+    if (showThumbnail) {
+      thumbnailRef.current.style.visibility = "visible";
+    } else {
+      thumbnailRef.current.style.visibility = "hidden";
+    }
+  });
   const Thumbnail = () => {
     return (
       <img
@@ -27,6 +35,9 @@ export default function VideoPlayer(props) {
         className={`
         ${styles.thumbnail}
         `}
+        // style={{
+        //   visibility: showThumbnail ? "visible" : "hidden"
+        // }}
         data-video={videoId}
         alt="Play this video"
         src={`https://img.youtube.com/vi/${videoId}/0.jpg`}
@@ -35,7 +46,50 @@ export default function VideoPlayer(props) {
   };
 
   return (
-    <div style={style} position={position} className={styles.body}>
+    <div
+      style={style}
+      position={position}
+      className={styles.body}
+      onClick={e => {
+        /* allows user to start video once loaded*/
+        playerLoading.then(player => {
+          try {
+            player.playVideo();
+          } catch {}
+        });
+      }}
+      onMouseEnter={e => {
+        /* Start loading youtube player and hide thumbnail*/
+        setLoadPlayer(true);
+        setShowThumbnail(false);
+
+        /* Will push items to left further when hovering on last item*/
+        if (position === "last") {
+          document.documentElement.style.setProperty(
+            "--slideTranslateMult",
+            `-1`
+          );
+        }
+      }}
+      onMouseOver={e => {
+        /* After re-render, if mouse is hovering over the video once the player is loaded, it will play*/
+        playerLoading.then(player => {
+          player.playVideo();
+        });
+      }}
+      onMouseLeave={e => {
+        /* If player is loaded, then pause when leaving slide and show thumbnail */
+        playerLoading.then(player => {
+          player.pauseVideo();
+          setShowThumbnail(true);
+        });
+
+        document.documentElement.style.setProperty(
+          "--slideTranslateMult",
+          `-2`
+        );
+      }}
+    >
       <Video
         id={videoId}
         setPlayer={setPlayer}
@@ -43,18 +97,28 @@ export default function VideoPlayer(props) {
         autoplay={autoplay}
         width={width}
       />
-      <Thumbnail />
-      <Overlay
-        type={type}
-        id={video._id}
-        player={player}
-        metaData={metaData}
-        thumbnailRef={thumbnailRef}
-        setShowThumbnail={setShowThumbnail}
-        setLoadPlayer={setLoadPlayer}
-        position={position}
-        showThumbnail={showThumbnail}
-      />
+      {type === "slide" ? (
+        <>
+          <Overlay
+            type={type}
+            id={video._id}
+            player={player}
+            metaData={metaData}
+          />
+          <Thumbnail />
+        </>
+      ) : (
+        <>
+          {" "}
+          <Thumbnail />
+          <Overlay
+            type={type}
+            id={video._id}
+            player={player}
+            metaData={metaData}
+          />
+        </>
+      )}
     </div>
   );
 }
